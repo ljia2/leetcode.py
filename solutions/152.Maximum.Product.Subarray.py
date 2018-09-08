40242557e7858150d757fd975791b591b77c093b
Jia, Lifeng <ljia@conversantmedia.com>
2018-09-07 20:04:17 -0500

    leetcode solutions

---
+++ solutions/152.Maximum.Product.Subarray.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProduct(self, nums):
+        """
+        Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+        Example 1:
+
+        Input: [2,3,-2,4]
+        Output: 6
+        Explanation: [2,3] has the largest product 6.
+        Example 2:
+
+        Input: [-2,0,-1]
+        Output: 0
+        Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+        :type nums: List[int]
+        :rtype: int
+        """
+        # dp[i] stores the max/min product of any subarray of nums[:i+1] but must using nums[i]
+        dp = [(0, 0)] * len(nums)
+        dp[0] = (nums[0], nums[0])
+        for i in range(1, len(nums), 1):
+            pre_max, pre_min = dp[i-1]
+            cmax = max(max(pre_max * nums[i], nums[i]), pre_min * nums[i])
+            cmin = min(min(pre_min * nums[i], nums[i]), pre_max * nums[i])
+            dp[i] = (cmax, cmin)
+        return max(map(lambda x: x[0], dp))
+
+s = Solution()
+print(s.maxProduct([-2,-1,0]))
---
+++ solutions/338.Counting.Bits.py
@@ -0,0 +1,38 @@
+class Solution:
+    def countBits(self, num):
+        """
+
+        Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+        Example 1:
+
+        Input: 2
+        Output: [0,1,1]
+        Example 2:
+
+        Input: 5
+        Output: [0,1,1,2,1,2]
+        Follow up:
+
+        It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+        Space complexity should be O(n).
+        Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+        :type num: int
+        :rtype: List[int]
+
+        First, we know that when a number * 2, it means this number shifts a bit in binary. And in every number + 1 case,
+        odd number always mean add 1 count in the 1’s count of the number in binary and we can know that:
+        answer[n] = answer[n >> 1] + (n & 1)
+        """
+
+        dp = [0]*(num + 1)
+        for i in range(1, num+1, 1):
+            if i == 1:
+                dp[i] = 1
+            else:
+                dp[i] = dp[i >> 1] + (i & 1)
+        return dp
+
+
+s = Solution()
+print(s.countBits(4))
