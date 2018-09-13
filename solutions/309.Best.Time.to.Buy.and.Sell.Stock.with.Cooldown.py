88c9c80ca560c7408d0f90fb05c264714b46239b
Jia, Lifeng <ljia@conversantmedia.com>
2018-09-12 22:34:05 -0500

    leetcode solutions

---
+++ solutions/309.Best.Time.to.Buy.and.Sell.Stock.with.Cooldown.py
@@ -0,0 +1,99 @@
+class Solution: # (TLE)
+    def maxProfit(self, prices):
+        """
+        Say you have an array for which the ith element is the price of a given stock on day i.
+
+        Design an algorithm to find the maximum profit. You may complete as many transactions as you
+        (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+        You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+        After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+        Example:
+
+        Input: [1,2,3,0,2]
+        Output: 3
+        Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+        :type prices: List[int]
+        :rtype: int
+
+        max_prifits[i] denotes the max profit up to day i
+        if max_profits[i] > max_profits[i-1] then sell a stock on day i otherwise no stock is sold on day i
+
+        max_profits[i] = max{ max_profits[k-1] + prices[i-1] - prices[k-1] | k from 1 to i (exclusive) } if max_profits[k-1] == max_profits[k-2]
+        or max{ max_profits[k-2] + prices[i-1] - prices[k-1] | k from 1 to i (exclusive) } if max_prifits[k-1] > max_profits[k-2]
+
+        """
+
+        if not prices or len(prices) < 2:
+            return 0
+        else:
+            if len(prices) == 2:
+                return prices[1] - prices[0] if prices[1] > prices[0] else 0
+            else:
+                max_profits = [0] * (len(prices) + 1)
+                max_profits[1] = 0
+                max_profits[2] = max(0, prices[1] - prices[0])
+                for i in range(3, len(prices)+1):
+                    # initialize the best buy in
+                    best_buy_in = max_profits[1] - prices[0]
+                    for k in range(2, i):
+                        if max_profits[k-1] == max_profits[k-2]:
+                                if max_profits[k-1] - prices[k-1] > best_buy_in:
+                                    best_buy_in = max_profits[k-1] - prices[k-1]
+                        else: # max_profits[k-1] > max_profits[k-2]
+                            if max_profits[k-2] - prices[k-1] > best_buy_in:
+                                best_buy_in = max_profits[k-2] - prices[k-1]
+
+                    max_profits[i] = max(max_profits[i-1], prices[i-1] + best_buy_in)
+                return max_profits[len(prices)]
+
+class BestSolution:
+    def maxProfit(self, prices):
+        """
+        Say you have an array for which the ith element is the price of a given stock on day i.
+
+        Design an algorithm to find the maximum profit. You may complete as many transactions as you
+        (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+        You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+        After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+        Example:
+
+        Input: [1,2,3,0,2]
+        Output: 3
+        Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+        :type prices: List[int]
+        :rtype: int
+
+        hint on one day you either can buy, sell or rest
+
+        when calculate the profit, we can assume hold stock (i.e. buy in stock, profit is negative stock price buyin
+        and sell stock the profit is plus the stock price sold.
+        """
+
+        if not prices or len(prices) < 2:
+            return 0
+        else:
+            hold = [0] * (len(prices) + 1)
+            hold[0] = - sum(prices)
+            sell = [0] * (len(prices) + 1)
+            rest = [0] * (len(prices) + 1)
+            for i in range(1, len(prices)+1):
+                # max profit of holding a stock on day i
+                # 1) holding a stock on day i - 1 and keep holding on day i
+                # 2) given having no stock on day i - 1, buy a stock on day i with a negative profit of -prices[i-1]
+                hold[i] = max(rest[i-1] - prices[i-1], hold[i-1])
+                # max profit of selling a stock on day i
+                # 1) holding a stock on day i - 1 plus the profit of selling a stock at prices[i-1]
+                sell[i] = hold[i-1] + prices[i-1]
+                # max profit of resting on day i (neither buy stock nor hold stock)
+                # 1) given having no stock on day i - 1, continue do nothing on day i
+                # 2) just sell a stock on day i-1 and have to cool down on day i
+                rest[i] = max(rest[i-1], sell[i-1])
+            return max(sell[-1], rest[-1])
+s = BestSolution()
+print(s.maxProfit([1,2,3,0,2]))
+
+
---
+++ solutions/413.Arithmetic.Slices.py
@@ -0,0 +1,98 @@
+class Solution: # TLE
+    def numberOfArithmeticSlices(self, A):
+        """
+        A sequence of number is called arithmetic if it consists of at least three elements
+        and if the difference between any two consecutive elements is the same.
+
+        For example, these are arithmetic sequence:
+
+        1, 3, 5, 7, 9
+        7, 7, 7, 7
+        3, -1, -5, -9
+        The following sequence is not arithmetic.
+
+        1, 1, 2, 5, 7
+
+        A zero-indexed array A consisting of N numbers is given. A slice of that array is any pair of integers (P, Q) such that 0 <= P < Q < N.
+
+        A slice (P, Q) of array A is called arithmetic if the sequence:
+        A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+
+        The function should return the number of arithmetic slices in the array A.
+
+
+        Example:
+
+        A = [1, 2, 3, 4]
+
+        return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+
+        :type A: List[int]
+        :rtype: int
+        """
+        if not A or len(A) == 0:
+            return 0
+        else:
+            slices = [[False] * len(A) for r in range(len(A)+1)]
+            for l in range(3, len(A)+1):
+                for i in range(len(A)-l+1):
+                    j = l + i - 1
+                    if l == 3:
+                        if A[i+1] - A[i] == A[j] - A[i+1]:
+                            slices[l][i] = True
+                    else:
+                        if slices[l-1][i] and A[i+1]-A[i] == A[i+l-1]-A[i+l-2]:
+                            slices[l][i] = True
+            return sum([sum(r) for r in slices])
+
+
+class BestSolution:
+    def numberOfArithmeticSlices(self, A):
+        """
+        A sequence of number is called arithmetic if it consists of at least three elements
+        and if the difference between any two consecutive elements is the same.
+
+        For example, these are arithmetic sequence:
+
+        1, 3, 5, 7, 9
+        7, 7, 7, 7
+        3, -1, -5, -9
+        The following sequence is not arithmetic.
+
+        1, 1, 2, 5, 7
+
+        A zero-indexed array A consisting of N numbers is given. A slice of that array is any pair of integers (P, Q) such that 0 <= P < Q < N.
+
+        A slice (P, Q) of array A is called arithmetic if the sequence:
+        A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+
+        The function should return the number of arithmetic slices in the array A.
+
+
+        Example:
+
+        A = [1, 2, 3, 4]
+
+        return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+
+        :type A: List[int]
+        :rtype: int
+        """
+        if not A or len(A) < 3:
+            return 0
+        else:
+            # dp[i] means the number of arithmetic slices ending with A[i]
+            dp = [0] * len(A)
+            if A[1] - A[0] == A[2] - A[1]:
+                dp[2] = 1
+            result = dp[2]
+            for i in range(3, len(A)):
+                if A[i] - A[i-1] == A[i-1]-A[i-2]:
+                    # the nubmer of a slices ending with i is equal to
+                    # the number of a-slices ending with i-1 after appending i + one additional a-slice of original length ending at i
+                    dp[i] = dp[i-1] + 1
+                result += dp[i]
+            return result
+
+s = BestSolution()
+print(s.numberOfArithmeticSlices([3, -1, -5, -9, -13]))
