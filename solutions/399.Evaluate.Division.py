2053b73a66e7f92be95b853cb730f9d64af17357
Jia, Lifeng <ljia@conversantmedia.com>
2018-10-19 09:58:28 -0500

    leetcode solutions 399 and 681

--- solutions/399.Evaluate.Division.py
+++ solutions/399.Evaluate.Division.py
@@ -1,11 +1,36 @@
-class Solution:
+class DFSSolution:
     def calcEquation(self, equations, values, queries):
         """
+        Equations are given in the format A / B = k, where A and B are variables represented as strings,
+        and k is a real number (floating point number).
+        Given some queries, return the answers. If the answer does not exist, return -1.0.
+
+        Example:
+        Given a / b = 2.0, b / c = 3.0.
+        queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
+        return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+
+        The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries ,
+        where equations.size() == values.size(), and the values are positive. This represents the equations. Return vector<double>.
+
+        According to the example above:
+
+        equations = [ ["a", "b"], ["b", "c"] ],
+        values = [2.0, 3.0],
+        queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ].
+        The input is always valid. You may assume that evaluating the queries will result in no division by zero and there is no contradiction.
+
+
         :type equations: List[List[str]]
         :type values: List[float]
         :type queries: List[List[str]]
         :rtype: List[float]
+
+        T: O(e + q*e)
+        S: O(e)
+
         """
+
         graph = dict()
         for index in range(len(equations)):
             a, b = equations[index][0], equations[index][1]
@@ -29,30 +54,25 @@ class Solution:
             elif src == dst:
                 results.append(1.0)
             else:
-                explored = [src]
-                results.append(self.searchGraph(graph, src, dst, explored))
+                explored = set()
+                results.append(self.dfs(graph, src, dst, explored))
         return results
 
-    def searchGraph(self, graph, src, dst, explored):
-        # check the direct edge
-        stack = []
+    def dfs(self, graph, src, dst, explored):
+        if src == dst:
+            return 1.0
+        # avoid cycle
+        explored.add(src)
         for (node, w) in graph[src]:
             if node in explored:
                 continue
             if node == dst:
                 return w
             else:
-                stack.append((node, w))
-        # DFS search
-        while stack:
-            new_src, w = stack.pop()
-            explored.append(new_src)
-            print(explored)
-            res = self.searchGraph(graph, new_src, dst, explored)
-            if res >= 0.0:
-                return w * res
+                res = self.dfs(graph, node, dst, explored)
+                if res >= 0.0:
+                    return w * res
         return -1.0
 
-
-s = Solution()
+s = DFSSolution()
 print(s.calcEquation([["a","b"],["b","c"],["c", "e"],["c", "d"]], [2.0, 3.0, 1.0, 4.0], [["d", "a"], ["e", "d"]]))
--- solutions/681.Next.Closest.Time.py
+++ solutions/681.Next.Closest.Time.py
@@ -1,8 +1,33 @@
-class Solution:
+class BruteForceSolution:
     def nextClosestTime(self, time):
         """
+        Given a time represented in the format "HH:MM", form the next closest time by reusing the current digits.
+        There is no limit on how many times a digit can be reused.
+
+        You may assume the given input string is always valid. For example, "01:34", "12:09" are all valid. "1:34", "12:9" are all invalid.
+
+        Example 1:
+
+        Input: "19:34"
+        Output: "19:39"
+        Explanation: The next closest time choosing from digits 1, 9, 3, 4, is 19:39, which occurs 5 minutes later.
+        It is not 19:33, because this occurs 23 hours and 59 minutes later.
+
+        Example 2:
+
+        Input: "23:59"
+        Output: "22:22"
+        Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 22:22.
+        It may be assumed that the returned time is next day's time since it is smaller than the input time numerically.
+        Seen this question in a real interview before?
+
         :type time: str
         :rtype: str
+
+        keep adding a minute until encouter a time reusing existing digits.
+
+        T: O(24*60)
+
         """
         digits = set()
         for d in time:
@@ -11,10 +36,12 @@ class Solution:
             digits.add(d)
         hour = int(time[:time.index(":")])
         min = int(time[time.index(":")+1:])
+
         while True:
             min = (min + 1) % 60
             if min == 0:
                 hour = (hour + 1) % 24
+            # make hour and minute valid
             h_str = "0" + str(hour) if hour < 10 else str(hour)
             m_str = "0" + str(min) if min < 10 else str(min)
             if self.digitCheck(digits, h_str, m_str):
@@ -33,7 +60,7 @@ class Solution:
 
 
 def main():
-    s = Solution()
+    s = BruteForceSolution()
     print(s.nextClosestTime("01:30"))
     print(s.nextClosestTime("23:59"))
 
