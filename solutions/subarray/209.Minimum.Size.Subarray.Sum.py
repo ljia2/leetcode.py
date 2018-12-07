2819a6c40df888af429ea4d15387b175e48e851a
Jia, Lifeng <ljia@conversantmedia.com>
2018-12-07 00:00:51 -0600

    subarrays solutions

--- solutions/subarray/209.Minimum.Size.Subarray.Sum.py
+++ solutions/subarray/209.Minimum.Size.Subarray.Sum.py
@@ -1,7 +1,52 @@
+class Solution: # Two Pointers solution
+    def minSubArrayLen(self, s, nums):
+        """
+        Given an array of n positive integers and a positive integer s,
+        find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+        Example:
+
+        Input: s = 7, nums = [2,3,1,2,4,3]
+        Output: 2
+        Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+        Follow up:
+        If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+
+        :type s: int
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if not nums or sum(nums) < s:
+            return 0
+
+        i = j = 0
+        j = 0
+        psum1 = 0
+        psum2 = nums[0]
+        min_length = len(nums) + 1
+        while i < len(nums) and j < len(nums):
+            ssum = psum2 - psum1
+            if ssum < s:
+                psum2 += nums[j]
+                j += 1
+            else:
+                length = j - i
+                if min_length < length:
+                    min_length = length
+                psum1 += nums[i]
+                i += 1
+
+
+
+
+
+######################################
+
 import bisect
 from collections import OrderedDict
 
-class Solution:
+class SolutionII:
     def minSubArrayLen(self, s, nums):
         """
         Given an array of n positive integers and a positive integer s,
@@ -45,6 +90,6 @@ class Solution:
                 min_length = length
         return min_length
 
-s = Solution()
+s = SolutionII()
 #print(s.minSubArrayLen(11, [1, 2, 3, 4, 5]))
 print(s.minSubArrayLen(7, [2,3,1,2,4,3]))
--- solutions/subarray/560.Subarray.Sum.Equals.K.py
+++ solutions/subarray/560.Subarray.Sum.Equals.K.py
@@ -1,55 +1,58 @@
-class Solution: # Brute Force
-    def subarraySum(self, nums, k):
-        """
+# class Solution: # Brute Force
+#     def subarraySum(self, nums, k):
+#         """
+#
+#         Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+#
+#         Example 1:
+#         Input:nums = [1,1,1], k = 2
+#         Output: 2
+#         Note:
+#         The length of the array is in range [1, 20,000].
+#         The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+#
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: int
+#         """
+#         count = 0
+#         for i in range(len(nums)):
+#             for j in range(i, len(nums)):
+#                 sum = 0
+#                 for k in range(i, j+1):
+#                     sum += nums[k]
+#                 if sum == k:
+#                     count += 1
+#         return count
+#
+# class SolutionII: # Brute Force without repeated computation
+#     def subarraySum(self, nums, k):
+#         """
+#
+#         Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+#
+#         Example 1:
+#         Input:nums = [1,1,1], k = 2
+#         Output: 2
+#         Note:
+#         The length of the array is in range [1, 20,000].
+#         The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+#
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: int
+#         """
+#         count = 0
+#         for i in range(len(nums)):
+#             presum = 0
+#             for j in range(i, len(nums)):
+#                 presum += nums[j]
+#                 if presum == k:
+#                     count += 1
+#         return count
 
-        Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+from collections import defaultdict
 
-        Example 1:
-        Input:nums = [1,1,1], k = 2
-        Output: 2
-        Note:
-        The length of the array is in range [1, 20,000].
-        The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
-
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        count = 0
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                sum = 0
-                for k in range(i, j+1):
-                    sum += nums[k]
-                if sum == k:
-                    count += 1
-        return count
-
-class SolutionII: # Brute Force without repeated computation
-    def subarraySum(self, nums, k):
-        """
-
-        Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
-
-        Example 1:
-        Input:nums = [1,1,1], k = 2
-        Output: 2
-        Note:
-        The length of the array is in range [1, 20,000].
-        The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
-
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        count = 0
-        for i in range(len(nums)):
-            presum = 0
-            for j in range(i, len(nums)):
-                presum += nums[j]
-                if presum == k:
-                    count += 1
-        return count
 
 class SolutionIII:
     def subarraySum(self, nums, k):
@@ -75,18 +78,15 @@ class SolutionIII:
         to find the subarray summing to k, i.e. prefix_sum[j] - prefix_sum[i] == k where j > i
 
         1. use hashtable to store the prefix sum of nums[:i] as key and indices list as value
-        2. scan the array by calcaulting prefix_sum of nums[:i], s and use k+s as key to query hashtable to ensure there exits j > i with the key
+        2. scan the array by calculating prefix_sum of nums[:i], s and use k+s as key to query hashtable to ensure there exits j > i with the key
         moreover, we need to check whether s == k or not.
         """
 
-        prefix_sum = dict()
+        prefix_sum = defaultdict(list)
         psum = 0
         for i in range(len(nums)):
             psum += nums[i]
-            if psum not in prefix_sum.keys():
-                prefix_sum[psum] = [i]
-            else:
-                prefix_sum[psum].append(i)
+            prefix_sum[psum].append(i)
 
         count = 0
         psum = 0
@@ -141,9 +141,10 @@ class SolutionIV:
             prefix_sum.append(psum)
 
         count = 0
-        # key: prefix_sum; value: the frequency of key before j (i.e. # of subarraies summing to key before j)
+        # key: prefix_sum;
+        # value: the frequency of key before j (i.e. # of subarraies summing to key before j)
         prefix_dict = dict()
-        # use to track the subarray starting with the first element summing to k
+        # use to track the subarrays starting with the first element summing to k
         prefix_dict[0] = 1
         for j in range(1, len(prefix_sum)):
             target = prefix_sum[j] - k
