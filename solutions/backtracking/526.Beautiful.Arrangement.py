bede2205ce846f2b7b67981a33e380bb77b2d70b
Jia, Lifeng <ljia@conversantmedia.com>
2019-01-04 18:34:12 -0600

    bt solution

--- solutions/backtracking/526.Beautiful.Arrangement.py
+++ solutions/backtracking/526.Beautiful.Arrangement.py
@@ -5,8 +5,7 @@ class Solution:
         that is constructed by these N numbers successfully
         if one of the following is true for the ith position (1 <= i <= N) in this array:
 
-        The number at the ith position is divisible by i.
-        i is divisible by the number at the ith position.
+        The number at the ith position is divisible by i. i is divisible by the number at the ith position.
 
         Now given N, how many beautiful arrangements can you construct?
 
@@ -17,21 +16,67 @@ class Solution:
         Explanation:
 
         The first beautiful arrangement is [1, 2]:
-
         Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
-
         Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
 
         The second beautiful arrangement is [2, 1]:
-
         Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
-
         Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1.
 
         Note:
-
         N is a positive integer and will not exceed 15.
 
         :type N: int
         :rtype: int
+
+        Typical usage of Permutation!
+
+        How about use permutation of N and determine whether it is feasible.
+
+        Trick!!!!!
+
+        Since position 1 can hold any number, we should not use top-down.
+        Instead, we should start from N to 1 (bottom-up).
+
         """
+        if not N:
+            return 0
+
+        if N <= 2:
+            return N
+
+        used = dict()
+        for i in range(1, N+1):
+            used[i] = False
+
+        ans = [0]
+        permutation = [None] * N
+        self.dfs(N, N-1, permutation, used, ans)
+        return ans[0]
+
+    def dfs(self, N, index, permutation, used, ans):
+        # EXIT: all positions have been filed.
+        if index == -1:
+            # count the number
+            ans[0] += 1
+            # if all possible results, we need to add  the copy of permutation to ans.
+            # ans.append(permutation.copy())
+            return
+
+        for n in range(1, N+1):
+            if used[n]:
+                continue
+            pos = index + 1
+            # try put number i at pos
+            if n % pos != 0 and pos % n != 0:
+                continue
+            #permutation[start_pos] = n
+            used[n] = True
+            # try to fill the position next to start_pos
+            self.dfs(N, index - 1, permutation, used, ans)
+            used[n] = False
+            #permutation[start_pos] = None
+        return
+
+s = Solution()
+print(s.countArrangement(15))
