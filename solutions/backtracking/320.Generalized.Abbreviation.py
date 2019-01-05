b485a9ee59a1a65643aeb0d18c4dd4b9c00318f0
Jia, Lifeng <ljia@conversantmedia.com>
2019-01-04 23:17:40 -0600

    backtracking solutions

---
+++ solutions/backtracking/320.Generalized.Abbreviation.py
@@ -0,0 +1,43 @@
+class Solution:
+    def generateAbbreviations(self, word):
+        """
+        Write a function to generate the generalized abbreviations of a word.
+
+        Note: The order of the output does not matter.
+
+        Example:
+
+        Input: "word"
+        Output:
+        ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+
+        :type word: str
+        :rtype: List[str]
+
+        typical bracktracking problem
+
+        for each position of a word:
+            1) abbreviate it with a number (count + 1)
+            2) or not to abbreviate it and keep it alphabetically.
+        """
+
+        ans = []
+        cur_abbr = ""
+        self.dfs(word, 0, cur_abbr, 0, ans)
+        return ans
+
+    def dfs(self, word, pos, cur_abbr, count, ans):
+        if len(word) == pos:
+            # Once we reach the end, append current to the result
+            abbr = cur_abbr + str(count) if count > 0 else cur_abbr
+            ans.append(abbr)
+            return
+
+        # Skip current position (abbreviate by count + 1), and increment count
+        self.dfs(word, pos + 1, cur_abbr, count + 1, ans)
+
+        # Include current position, and zero-out count
+        new_cur_abbr = cur_abbr + (str(count) if count > 0 else '') + word[pos]
+        self.dfs(word, pos + 1, new_cur_abbr, 0, ans)
+
+        return
---
+++ solutions/backtracking/411.Minimum.Unique.Word.Abbreviation.py
@@ -0,0 +1,72 @@
+from heapq import heappush, heappop
+class Solution:
+    def minAbbreviation(self, target, dictionary):
+        """
+        A string such as "word" contains the following abbreviations:
+
+        ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+        Given a target string and a set of strings in a dictionary,
+        find an abbreviation of this target string with the smallest possible length
+        such that it does not conflict with abbreviations of the strings in the dictionary.
+
+        Each number or letter in the abbreviation is considered length = 1. For example, the abbreviation "a32bc" has length = 4.
+
+        Note:
+
+        In the case of multiple answers as shown in the second example below, you may return any one of them.
+        Assume length of target string = m, and dictionary size = n. You may assume that m ≤ 21, n ≤ 1000, and log2(n) + m ≤ 20.
+
+        Examples:
+
+        "apple", ["blade"] -> "a4" (because "5" or "4e" conflicts with "blade")
+
+        "apple", ["plain", "amber", "blade"] -> "1p3" (other valid answers include "ap3", "a3e", "2p2", "3le", "3l1").
+
+        :type target: str
+        :type dictionary: List[str]
+        :rtype: str
+
+        backtracking to get all abbreviations of a word.
+        just check abbreviations of target with all abbreviations of dictionary words.
+
+        """
+
+        if not dictionary:
+            return str(len(target))
+
+        abbr_set = set()
+        for w in dictionary:
+            abbrs = self.generate_abbr(w)
+            for abbr in abbrs:
+                abbr_set.add(abbr)
+
+        abbr_pq = []
+        abbrs = self.generate_abbr(target)
+        for abbr in abbrs:
+            l = len(abbr)
+            heappush(abbr_pq, (l, abbr))
+
+        while abbr_pq:
+            _, abbr = heappop(abbr_pq)
+            if abbr not in abbr_set:
+                return abbr
+
+    def generate_abbr(self, word):
+        ans = []
+        cur_abbr = ""
+        self.dfs(word, 0, 0, cur_abbr, ans)
+        return ans
+
+    def dfs(self, word, pos, count, cur_abbr, ans):
+        if pos == len(word):
+            abbr = (cur_abbr + str(count)) if count > 0 else cur_abbr
+            ans.append(abbr)
+            return
+
+        self.dfs(word, pos+1, count+1, cur_abbr, ans)
+        new_cur_abbr = (cur_abbr + str(count) + word[pos]) if count > 0 else (cur_abbr + word[pos])
+        self.dfs(word, pos+1, 0, new_cur_abbr, ans)
+        return
+
+s = Solution()
+print(s.minAbbreviation("usaandchinaarefriends", []))
