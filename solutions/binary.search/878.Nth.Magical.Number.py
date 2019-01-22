52be929aca3ac4c66043476ea67335d0aa06383d
Jia, Lifeng <ljia@conversantmedia.com>
2019-01-21 23:12:49 -0600

    binary search solutions

---
+++ solutions/binary.search/878.Nth.Magical.Number.py
@@ -0,0 +1,73 @@
+class Solution:
+    def nthMagicalNumber(self, N, A, B):
+        """
+        A positive integer is magical if it is divisible by either A or B.
+
+        Return the N-th magical number.  Since the answer may be very large, return it modulo 10^9 + 7.
+
+        Example 1:
+
+        Input: N = 1, A = 2, B = 3
+        Output: 2
+        Example 2:
+
+        Input: N = 4, A = 2, B = 3
+        Output: 6
+        Example 3:
+
+        Input: N = 5, A = 2, B = 4
+        Output: 10
+        Example 4:
+
+        Input: N = 3, A = 6, B = 4
+        Output: 8
+
+        :type N: int
+        :type A: int
+        :type B: int
+        :rtype: int
+
+        4 points to figure out:
+
+        1. Get gcd (greatest common divisor) and lcm (least common multiple) of (A, B).
+            (a, b) = (A, B) while b > 0: (a, b) = (b, a % b)
+            then, gcd = a and lcm = A * B / a
+
+        2. How many magic numbers <= x ?
+            By inclusion exclusion principle, we have: x / A + x / B - x / lcm
+
+        3. Set our binary search range
+            Lower bound is min(A, B), I just set left = 2.
+            Upper bound is N * min(A, B), I just set right = 10 ^ 14.
+
+        4. binary search, find the smallest x that x / A + x / B - x / lcm = N
+        """
+
+        l = 2
+        r = 10 ** 14
+        mod = 10 ** 9 + 7
+        lcm = A * B // self.gcd(A, B)
+        # find the smallest number (lower bound) where mid // A + mid // B - mid // lcm = N, similar to bisect_left
+        while l < r:
+            mid = (l + r) // 2
+            tmp = mid // A + mid // B - mid // lcm
+            if tmp >= N:
+                r = mid
+            else:
+                l = mid + 1
+        return l % mod
+
+    def gcd(self, A, B):
+        if B == 0:
+            return A
+
+        if A < B:
+            return self.gcd(B, A)
+
+        return self.gcd(B, A % B)
+
+
+s = Solution()
+print(s.nthMagicalNumber(10, 6, 4))
+print(s.nthMagicalNumber(1, 2, 3))
+print(s.nthMagicalNumber(5, 2, 4))
---
+++ solutions/binary.search/911.Online.Election.py
@@ -0,0 +1,79 @@
+"""
+In an election, the i-th vote was cast for persons[i] at time times[i].
+
+Now, we would like to implement the following query function: TopVotedCandidate.q(int t) will return the number of the person that was leading the election at time t.
+
+Votes cast at time t will count towards our query.  In the case of a tie, the most recent vote (among tied candidates) wins.
+
+
+
+Example 1:
+
+Input: ["TopVotedCandidate","q","q","q","q","q","q"], [[[0,1,1,0,0,1,0],[0,5,10,15,20,25,30]],[3],[12],[25],[15],[24],[8]]
+Output: [null,0,1,1,0,0,1]
+Explanation:
+At time 3, the votes are [0], and 0 is leading.
+At time 12, the votes are [0,1,1], and 1 is leading.
+At time 25, the votes are [0,1,1,0,0,1], and 1 is leading (as ties go to the most recent vote.)
+This continues for 3 more queries at time 15, 24, and 8.
+
+
+Note:
+
+1 <= persons.length = times.length <= 5000
+0 <= persons[i] <= persons.length
+times is a strictly increasing array with all elements in [0, 10^9].
+TopVotedCandidate.q is called at most 10000 times per test case.
+TopVotedCandidate.q(int t) is always called with t >= times[0].
+"""
+
+import bisect
+
+class TopVotedCandidate:
+
+    def __init__(self, persons, times):
+        """
+        :type persons: List[int]
+        :type times: List[int]
+        """
+
+        self.winners = []
+        self.times = times
+        p2count = dict()
+        max_p = -1
+        for i, t in enumerate(times):
+            p = persons[i]
+            if max_p < 0:
+                max_p = p
+                p2count[p] = 1
+            else:
+                c = p2count.get(p, 0)
+                if p == max_p:
+                    p2count[p] = c + 1
+                else:
+                    if c + 1 < p2count[max_p]:
+                        p2count[p] = c+1
+                    else:
+                        max_p = p
+                        p2count[p] = c+1
+            self.winners.append(max_p)
+
+
+    def q(self, t):
+        """
+        :type t: int
+        :rtype: int
+        """
+        i = bisect.bisect_right(self.times, t)
+        if i > 0:
+            return self.winners[i-1]
+        else:
+            raise RuntimeError("""time $t is invalid! """)
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
+
+obj = TopVotedCandidate([0,0,0,0,1],[0,6,39,52,75])
+obj.q(99)
