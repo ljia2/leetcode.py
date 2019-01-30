0774913e56e8089fc4851765a37a111299377df6
Jia, Lifeng <ljia@conversantmedia.com>
2019-01-30 08:51:25 -0600

    tree solution

--- solutions/tree/979.Distribute.Coins.in.Binary.Tree.py
+++ solutions/tree/979.Distribute.Coins.in.Binary.Tree.py
@@ -40,7 +40,40 @@ class Solution:
         1<= N <= 100
         0 <= node.val <= N
 
+        https://leetcode.com/problems/distribute-coins-in-binary-tree/description/
+
         :type root: TreeNode
         :rtype: int
+
+        calculate the flow on each edge of tree.
+
+        give an root with a left and right tree.
+
+        the balance value of root is 1) zero: coins are balanced one per node
+                                     2) positive (more coins than one per node)
+                                     3) negative (less coins than one per node)
+
+        balance(root) = root.val - 1 + balance(left) + balance(right)
+
+        for left-root and right-root edges: there are abs(balance(left)) moves and abs(balance(right)) moves needed.
+
+        ans += abs(balance(left)) + abs(balance(right))
+
         """
+
+        if not root:
+            return 0
+
+        ans = [0]
+        _ = self.balance(root, ans)
+        return ans[0]
+
+    # return the positive number of extra coins or the negative number of needed coins.
+    def balance(self, root, ans):
+        if not root:
+            return 0
+        left_bal = self.balance(root.left, ans) if root.left else 0
+        right_bal = self.balance(root.right, ans) if root.right else 0
+        ans[0] += abs(left_bal) + abs(right_bal)
+        return root.val - 1 + left_bal + right_bal
 
