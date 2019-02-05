f779736bc2bcecc7a26fd38172b9792d6549ce78
Jia, Lifeng <ljia@conversantmedia.com>
2019-02-05 16:28:25 -0600

    tree solutions

---
+++ solutions/tree/987.Vertical.Order.Traversal.of.a.Binary.Tree.py
@@ -0,0 +1,83 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from heapq import heappop, heappush
+
+class Solution:
+    """
+    Given a binary tree, return the vertical order traversal of its nodes values.
+
+    For each node at position (X, Y), its left and right children respectively will be at positions (X-1, Y-1) and (X+1, Y-1).
+
+    Running a vertical line from X = -infinity to X = +infinity, whenever the vertical line touches some nodes, we report the values of the nodes in order from top to bottom (decreasing Y coordinates).
+
+    If two nodes have the same position, then the value of the node that is reported first is the value that is smaller.
+
+    Return an list of non-empty reports in order of X coordinate.  Every report will have a list of values of nodes.
+
+
+
+    Example 1:
+
+
+
+    Input: [3,9,20,null,null,15,7]
+    Output: [[9],[3,15],[20],[7]]
+    Explanation:
+    Without loss of generality, we can assume the root node is at position (0, 0):
+    Then, the node with value 9 occurs at position (-1, -1);
+    The nodes with values 3 and 15 occur at positions (0, 0) and (0, -2);
+    The node with value 20 occurs at position (1, -1);
+    The node with value 7 occurs at position (2, -2).
+    Example 2:
+
+
+
+    Input: [1,2,3,4,5,6,7]
+    Output: [[4],[2],[1,5,6],[3],[7]]
+    Explanation:
+    The node with value 5 and the node with value 6 have the same position according to the given scheme.
+    However, in the report "[1,5,6]", the node value of 5 comes first since 5 is smaller than 6.
+
+
+    Note:
+
+    The tree will have between 1 and 1000 nodes.
+    Each node's value will be between 0 and 1000.
+
+
+    T: O(nlogn)
+    S: O(n)
+    """
+    def verticalTraversal(self, root: 'TreeNode') -> 'List[List[int]]':
+        if not root:
+            return []
+        nodehp = []
+        self.dfs(root, nodehp, 0, 0)
+        ans = []
+        while nodehp:
+            cur_line = []
+            x, y, val = heappop(nodehp)
+            cur_line.append(val)
+            while nodehp:
+                nx, ny, nval = heappop(nodehp)
+                if nx == x:
+                    cur_line.append(nval)
+                else:
+                    # push back the new val and break
+                    heappush(nodehp, (nx, ny, nval))
+                    break
+            ans.append(cur_line)
+        return ans
+
+    def dfs(self, root, hq, x, y):
+        heappush(hq, (x, -y, root.val))
+        if root.left:
+            self.dfs(root.left, hq, x-1, y-1)
+        if root.right:
+            self.dfs(root.right, hq, x+1, y-1)
+        return
---
+++ solutions/tree/988.Smallest.String.Starting.From.Leaf.py
@@ -0,0 +1,81 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    """
+    Given the root of a binary tree, each node has a value from 0 to 25 representing the letters 'a' to 'z':
+    a value of 0 represents 'a', a value of 1 represents 'b', and so on.
+
+    Find the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+
+    (As a reminder, any shorter prefix of a string is lexicographically smaller: for example, "ab" is lexicographically smaller than "aba".
+    A leaf of a node is a node that has no children.)
+
+
+
+    Example 1:
+
+
+
+    Input: [0,1,2,3,4,3,4]
+    Output: "dba"
+    Example 2:
+
+
+
+    Input: [25,1,3,1,3,0,2]
+    Output: "adz"
+    Example 3:
+
+
+
+    Input: [2,2,1,null,1,0,null,0]
+    Output: "abc"
+
+
+    Note:
+
+    The number of nodes in the given tree will be between 1 and 1000.
+    Each node in the tree will have a value between 0 and 25.
+    """
+    def smallestFromLeaf(self, root: 'TreeNode') -> 'str':
+        if not root:
+            return None
+        ans = [None]
+        self.dfs(root, "", ans)
+        return ans[0]
+
+    def dfs(self, root, path, ans):
+        if not root.left and not root.right:
+            # update the answer so far
+            self.minStr(root, path, ans)
+            return
+        curr_path = chr(ord('a') + root.val) + path
+        if root.left:
+            self.dfs(root.left, curr_path, ans)
+        if root.right:
+            self.dfs(root.right, curr_path, ans)
+        return
+
+    def minStr(self, root, curr_path, ans):
+        path = chr(ord('a') + root.val) + curr_path
+
+        if not ans[0]:
+            ans[0] = path
+        else:
+            # You can use ( > , < , <= , <= , == , !=  ) to compare two strings.
+            # Python compares string lexicographically i.e using ASCII value of the characters.
+            ans[0] = path if path < ans[0] else ans[0]
+        return
+
+s = Solution()
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+print(s.smallestFromLeaf(root))
