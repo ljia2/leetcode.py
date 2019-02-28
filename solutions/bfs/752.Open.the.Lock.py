e1d8216874926d3dfa984fc1ccac60750b90b8c3
Jia, Lifeng <ljia@conversantmedia.com>
2019-02-27 21:21:36 -0600

    bfs solution

--- solutions/bfs/752.Open.the.Lock.py
+++ solutions/bfs/752.Open.the.Lock.py
@@ -44,7 +44,81 @@ class Solution(object):
         The length of deadends will be in the range [1, 500].
         target will not be in the list deadends.
         Every string in deadends and the string target will be a string of 4 digits from the 10,000 possibilities '0000' to '9999'.
+
         :type deadends: List[str]
         :type target: str
         :rtype: int
+
+        typical usage of bfs on lock
+
+        TLE on BFS
+        OK for BiBFS
+
         """
+
+        if not target:
+            return 0
+        start = "0000"
+        #return self.bfs(start, target, deadends)
+        return self.bibfs(start, target, deadends)
+
+    def bibfs(self, start, target, deadends):
+        p = [start]
+        q = [target]
+        visited = set()
+        step = 0
+        while p and q:
+            if len(p) < len(q):
+                p, q = q, p
+            size = len(q)
+            while size > 0:
+                state = q.pop(0)
+                size -= 1
+
+                if state in p:
+                    return step
+
+                if state in deadends or state in visited:
+                    continue
+
+                visited.add(state)
+                q += self.next_states(state)
+            step += 1
+        return -1
+
+    def bfs(self, start, target, deadends):
+        q = [start]
+        visited = set()
+        step = 0
+        while q:
+            size = len(q)
+            while size > 0:
+                state = q.pop(0)
+                size -= 1
+
+                if state == target:
+                    return step
+
+                if state in deadends or state in visited:
+                    continue
+
+                visited.add(state)
+                q += self.next_states(state)
+
+            step += 1
+        return -1
+
+    def next_states(self, state):
+        states = []
+        for i, d in enumerate(state):
+            if int(d) == 0:
+                states.append(state[:i] + "1" + state[i+1:])
+                states.append(state[:i] + "9" + state[i+1:])
+            else:
+                states.append(state[:i] + str((int(d) + 1) % 10) + state[i+1:])
+                states.append(state[:i] + str((int(d) - 1) % 10) + state[i+1:])
+        return states
+
+
+s = Solution()
+print(s.openLock(["0201","0101","0102","1212","2002"], "0202"))
