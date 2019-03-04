7a9c11eae971f5c01ad5444de64781e6363ba555
Jia, Lifeng <ljia@conversantmedia.com>
2019-03-04 11:50:30 -0600

    994 bfs solution

--- solutions/bfs/994.Rotting.Oranges.py
+++ solutions/bfs/994.Rotting.Oranges.py
@@ -33,4 +33,57 @@ class Solution(object):
 
         :type grid: List[List[int]]
         :rtype: int
+
+        typical bfs over the grid.
+
+
         """
+        if not grid or not grid[0]:
+            return 0
+
+        q = []
+        visited = set()
+        fresh_count = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                    visited.add((r, c))
+                elif grid[r][c] == 1:
+                    fresh_count += 1
+        if not q:
+            return 0 if fresh_count == 0 else -1
+
+        if fresh_count == 0:
+            return 0
+
+        minutes = 0
+        while q:
+            size = len(q)
+            while size > 0:
+                r, c = q.pop(0)
+                size -= 1
+
+                # when visiting them, they got rotten.
+                if grid[r][c] == 1:
+                    fresh_count -= 1
+                if fresh_count == 0:
+                    return minutes
+
+                for nr, nc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
+                    if nr < 0 or nc < 0 or nr >= len(grid) or nc >= len(grid[0]) or grid[nr][nc] == 0:
+                        continue
+                    if (nr, nc) in visited:
+                        continue
+
+                    visited.add((nr, nc))
+                    q.append((nr, nc))
+            minutes += 1
+
+        return -1
+
+s = Solution()
+print(s.orangesRotting([[2,1,1],[1,1,0],[0,1,1]]))
+print(s.orangesRotting([[2,1,1],[0,1,1],[1,0,1]]))
+print(s.orangesRotting([[1],[2],[2]]))
+
